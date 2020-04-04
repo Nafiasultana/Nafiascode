0f58bb7d883c10603fce53b8adce26957c83a2eb
Nafiasultana <60855567+Nafiasultana@users.noreply.github.com>
2020-04-04 19:46:24 +0600

    Add files via upload

---
+++ conditionalstatement.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n==1) {cout<<"One"<<endl;}
+     else if (n==2) {cout<<"two"<<endl;}
+     else if (n==3) {cout<<"three"<<endl;}
+     else if (n==4) {cout<<"four"<<endl;}
+     else if (n==5) {cout<<"Five"<<endl;}
+     else if (n==6) {cout<<"Six"<<endl;}
+     else if (n==7) {cout<<"Seven"<<endl;}
+     else if (n==8) {cout<<"Eight"<<endl;}
+     else if (n==9) {cout<<"Nine"<<endl;}
+     else {cout<<"Greater than 9"<<endl;}
+     return 0;
+}
