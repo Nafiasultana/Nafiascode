009d12d93399da7654ade14a65a874456503b21c
Nafiasultana <60855567+Nafiasultana@users.noreply.github.com>
2020-04-04 19:43:09 +0600

    Add files via upload

---
+++ inpotp.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int firstNumber,secondNumber,thirdNumber,sumOfNumbers;
+    cout<<"";
+    cin>>firstNumber>>secondNumber>>thirdNumber;
+    sumOfNumbers=firstNumber+secondNumber+thirdNumber;
+    cout<<""<<sumOfNumbers;
+    return 0;
+}
